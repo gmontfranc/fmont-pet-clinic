fcd6e6bcc57f7dafa7a162f61e0d13e96a088418
Montilla Francisco <montilla2808@gmail.com>
2020-10-04 20:21:44 +0200

    Listing vets on vet page. Closes #6

--- pet-clinic-web/src/main/java/com/fmont/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/fmont/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.fmont.petclinic.controllers;
 
+import com.fmont.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vet","/vet/index.html","/vet/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "/vet/index";
     }
 }
