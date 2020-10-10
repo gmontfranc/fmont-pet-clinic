6f9d3dd55a7c08a3715779a5b44fe3be25721bc4
Montilla Francisco <montilla2808@gmail.com>
2020-10-10 17:01:14 +0200

    Creating visit entity (POJO). Closes #12

--- pet-clinic-data/src/main/java/com/fmont/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/fmont/petclinic/model/Owner.java
@@ -1,7 +1,21 @@
 package com.fmont.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+        pets = new HashSet<>();
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
