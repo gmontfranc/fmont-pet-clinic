d74603b3f3bd32ab336a40f319926943d55479b7
Montilla Francisco <montilla2808@gmail.com>
2020-10-10 17:05:50 +0200

    Creating specialty POJO and relationship with Vet. Closes #13

--- pet-clinic-data/src/main/java/com/fmont/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/fmont/petclinic/model/Vet.java
@@ -1,8 +1,21 @@
 package com.fmont.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Specialty> specialties;
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+        specialties = new HashSet<>();
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
