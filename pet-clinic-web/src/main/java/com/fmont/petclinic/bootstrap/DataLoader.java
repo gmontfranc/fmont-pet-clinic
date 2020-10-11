cbba2c6bc6f02accb1a12f44860e72a4426586b5
Montilla Francisco <montilla2808@gmail.com>
2020-10-11 14:43:33 +0200

    Adding pet types on startup. Closes #18

--- pet-clinic-web/src/main/java/com/fmont/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/fmont/petclinic/bootstrap/DataLoader.java
@@ -1,7 +1,9 @@
 package com.fmont.petclinic.bootstrap;
 
 import com.fmont.petclinic.model.Owner;
+import com.fmont.petclinic.model.PetType;
 import com.fmont.petclinic.services.OwnerService;
+import com.fmont.petclinic.services.PetTypeService;
 import com.fmont.petclinic.services.VetService;
 import com.fmont.petclinic.model.Vet;
 
@@ -14,16 +16,29 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Owner fred = new Owner("Fred","Delmotte");
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Car");
+
+        PetType savedCat = petTypeService.save(cat);
+
+
+        Owner fred = new Owner("Fred", "Delmotte");
 
         ownerService.save(fred);
 
@@ -33,11 +48,11 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Loaded Owners...");
 
-        Vet louise = new Vet("Louise","Unkwown");
+        Vet louise = new Vet("Louise", "Unkwown");
 
         vetService.save(louise);
 
-        Vet nicolas = new Vet("Nicolas","GF");
+        Vet nicolas = new Vet("Nicolas", "GF");
 
         vetService.save(nicolas);
 
