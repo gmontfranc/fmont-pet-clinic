810993bc0a8d268feda8179a1a8e96f00a0f6679
Montilla Francisco <montilla2808@gmail.com>
2020-10-21 20:00:22 +0200

    Updated vet class to JPA entity. Closes #23

--- pet-clinic-data/src/main/java/com/fmont/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/fmont/petclinic/model/Vet.java
@@ -1,11 +1,19 @@
 package com.fmont.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties;
+
+
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
         specialties = new HashSet<>();
