d1e2c5e9e1128bd2cfff6154fe98b2a67f712c5b
Montilla Francisco <montilla2808@gmail.com>
2020-10-21 19:52:51 +0200

    Converting owners to JPA entity and adapted relationships. Closes #22

--- pet-clinic-data/src/main/java/com/fmont/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/fmont/petclinic/model/Owner.java
@@ -1,15 +1,26 @@
 package com.fmont.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
+
+
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
         pets = new HashSet<>();
