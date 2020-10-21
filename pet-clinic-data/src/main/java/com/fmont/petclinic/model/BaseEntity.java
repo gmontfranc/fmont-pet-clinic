cdab4b72858abf1f4bd6727347e9e2d597489bf1
Montilla Francisco <montilla2808@gmail.com>
2020-10-21 18:52:52 +0200

    Adding base entity class

--- pet-clinic-data/src/main/java/com/fmont/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/fmont/petclinic/model/BaseEntity.java
@@ -1,12 +1,18 @@
 package com.fmont.petclinic.model;
 
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.util.Objects;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
